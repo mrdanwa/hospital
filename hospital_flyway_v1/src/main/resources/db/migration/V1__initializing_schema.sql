7fa77cf738e812043f3faf045ea642dc18f8e4f1
mrdanwa <111616219+mrdanwa@users.noreply.github.com>
2024-10-24 00:14:51 +0200

    Delete hospital_flyway_v1 directory

--- hospital_flyway_v1/src/main/resources/db/migration/V1__initializing_schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE SEQUENCE paciente_sequence START WITH 1 INCREMENT BY 1;
-
-CREATE TABLE paciente (
-      id BIGINT PRIMARY KEY DEFAULT NEXTVAL('paciente_sequence'),
-      dni VARCHAR(50) NOT NULL,
-      nombre VARCHAR(100) NOT NULL,
-      email VARCHAR(100) NOT NULL,
-      fecha_nacimiento DATE NOT NULL
-);
